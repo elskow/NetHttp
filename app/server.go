5f04fc3fe33d5e612e71dca4c06a0c0b0ab50238
elskow <elskow@outlook.com>
2024-07-05 20:25:09 +0700

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -3,6 +3,12 @@ package main
 import (
 	"log"
 	"net"
+	"strings"
+)
+
+const (
+	OK        = "HTTP/1.1 200 OK\r\n\r\n"
+	NOT_FOUND = "HTTP/1.1 404 Not Found\r\n\r\n"
 )
 
 func main() {
@@ -27,7 +33,28 @@ func main() {
 func handleRequest(conn net.Conn) {
 	defer conn.Close()
 
-	response := "HTTP/1.1 200 OK\r\n\r\n"
-	conn.Write([]byte(response))
-	log.Printf("Response: %s", response)
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	if n == 0 {
+		return
+	}
+
+	req := string(buf[:n])
+	path := extractUrlPath(req)
+
+	if path == "/" {
+		conn.Write([]byte(OK))
+	} else {
+		conn.Write([]byte(NOT_FOUND))
+	}
+}
+
+func extractUrlPath(req string) string {
+	return strings.Split(req, " ")[1]
 }
